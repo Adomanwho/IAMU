fa2f4688ff3356eafa8faeee08900844b235fcc7
Andrej Kolega <akolega@algebra.hr>
2026-02-13 21:52:19 +0100

    Display recycler view (bez remove i opisa).

--- app/src/main/java/com/example/pokemon/fragment/ItemsFragment.kt
+++ app/src/main/java/com/example/pokemon/fragment/ItemsFragment.kt
@@ -5,16 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.pokemon.R
+import com.example.pokemon.adapter.ItemAdapter
+import com.example.pokemon.databinding.FragmentItemsBinding
+import com.example.pokemon.framework.fetchItems
+import com.example.pokemon.model.Item
 
 class ItemsFragment : Fragment() {
+
+    private lateinit var binding: FragmentItemsBinding
+    private lateinit var items: MutableList<Item>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentItemsBinding.inflate(inflater, container, false)
+        items = requireContext().fetchItems()
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_items, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.rvItems.apply {
+            adapter = ItemAdapter(requireContext(), items)
+            layoutManager = LinearLayoutManager(requireContext())
+        }
     }
 
 }
